2d4452d5a4d760179b044a886a253b4a9cc25af0
George Fabunmi <otegbeyegeorge1@gmail.com>
2025-11-29 17:30:40 +0100

    Fix certain errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import ProductViewer from "./components/ProductViewer";
 
 const App = () => {
   return (
     <main>
       <NavBar />
-      <Hero />
-      <ProductViewer />
     </main>
   );
 };
